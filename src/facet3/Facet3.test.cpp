86588ce30cf15b1244e7a277dd8373346b2bbf16
Igor Trishin <yaschur@gmail.com>
2019-04-05 17:56:46 +0300

    add cmake flow

--- src/facet3/Facet3.test.cpp
+++ src/facet3/Facet3.test.cpp
@@ -2,7 +2,9 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-TEST(testFacet, drawTest) {
+class Facet3Tests : public testing::Test {};
+
+TEST(Facet3Tests, draw) {
   Facet3 f1(1, 2);
   f1.setLines(0b101, 0, 0b010);
   uint8_t matrix[] = {0b01000010, 0b01000101, 0b01111101, 0b01000101, 0b01000101, 0b01010101, 0b01011101, 0b11000101};
