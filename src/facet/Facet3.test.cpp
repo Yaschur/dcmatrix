8f9d2c2b0bc824b2a42d7996e0539dc3e216f8a5
Igor Trishin <i.trishin@snegirsoft.com>
2019-04-05 13:48:52 +0300

    add gtest, move unit test

---
+++ src/facet/Facet3.test.cpp
@@ -0,0 +1,14 @@
+#include "Facet3.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(testFacet, drawTest) {
+  Facet3 f1(1, 2);
+  f1.setLines(0b101, 0, 0b010);
+  uint8_t matrix[] = {0b01000010, 0b01000101, 0b01111101, 0b01000101, 0b01000101, 0b01010101, 0b01011101, 0b11000101};
+  uint8_t extrix[] = {0b01000010, 0b01101101, 0b01000101, 0b01010101, 0b01000101, 0b01010101, 0b01011101, 0b11000101};
+
+  f1.drawTo(matrix);
+
+  ASSERT_THAT(matrix, testing::ElementsAreArray(extrix));
+}
