ace303b6f765321aa4e2a702cd3240ca023cf6db
Igor Trishin <yaschur@gmail.com>
2019-04-10 16:18:05 +0300

    add facets placeholder to dcmatrix

--- include/dcmatrix/Dcmatrix.hpp
+++ include/dcmatrix/Dcmatrix.hpp
@@ -2,14 +2,16 @@
 #include <array>
 #include <cstdint>
 #include <utility>
+#include "Facet3.hpp"
 using namespace std;
 
 class Dcmatrix {
  public:
   explicit Dcmatrix(const array<uint8_t, 8> &rowPinsInOrder, const array<uint8_t, 8> &colPinsInOrder);
+  ~Dcmatrix();
   pair<uint8_t, array<pair<uint8_t, bool>, 8>> getLine(uint8_t lineNum);
 
  private:
-  array<uint8_t, 8> _rowPins;
-  array<uint8_t, 8> _colPins;
+  array<uint8_t, 8> _rowPins, _colPins;
+  array<Facet3*, 4> _facets;
 };
--- src/dcmatrix/Dcmatrix.cpp
+++ src/dcmatrix/Dcmatrix.cpp
@@ -1,7 +1,14 @@
 #include "Dcmatrix.hpp"
 
 Dcmatrix::Dcmatrix(const array<uint8_t, 8> &rowPinsInOrder, const array<uint8_t, 8> &colPinsInOrder)
-    : _rowPins(rowPinsInOrder), _colPins(colPinsInOrder) {}
+    : _rowPins(rowPinsInOrder), _colPins(colPinsInOrder), _facets({nullptr, nullptr, nullptr, nullptr}) {}
+
+Dcmatrix::~Dcmatrix() {
+  for (auto f : _facets) {
+    if (!f) continue;
+    delete f;
+  }
+}
 
 pair<uint8_t, array<pair<uint8_t, bool>, 8>> Dcmatrix::getLine(uint8_t lineNum) {
   array<pair<uint8_t, bool>, 8> cols = {make_pair(0, false), make_pair(0, false), make_pair(0, false),
