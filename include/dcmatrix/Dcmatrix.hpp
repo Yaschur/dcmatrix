00b17dfdcd9e2a40e23b02a9d1b05f31d80689f2
Igor Trishin <yaschur@gmail.com>
2019-04-11 09:37:54 +0300

    make it great again

--- include/dcmatrix/Dcmatrix.hpp
+++ include/dcmatrix/Dcmatrix.hpp
@@ -8,10 +8,9 @@ using namespace std;
 class Dcmatrix {
  public:
   explicit Dcmatrix(const array<uint8_t, 8> &rowPinsInOrder, const array<uint8_t, 8> &colPinsInOrder);
-  ~Dcmatrix();
   pair<uint8_t, array<pair<uint8_t, bool>, 8>> getLine(uint8_t lineNum);
 
  private:
   array<uint8_t, 8> _rowPins, _colPins;
-  array<Facet3*, 4> _facets;
+  array<Facet3, 4> _facets;
 };
--- src/dcmatrix/Dcmatrix.cpp
+++ src/dcmatrix/Dcmatrix.cpp
@@ -1,14 +1,9 @@
 #include "Dcmatrix.hpp"
 
 Dcmatrix::Dcmatrix(const array<uint8_t, 8> &rowPinsInOrder, const array<uint8_t, 8> &colPinsInOrder)
-    : _rowPins(rowPinsInOrder), _colPins(colPinsInOrder), _facets({nullptr, nullptr, nullptr, nullptr}) {}
-
-Dcmatrix::~Dcmatrix() {
-  for (auto f : _facets) {
-    if (!f) continue;
-    delete f;
-  }
-}
+    : _rowPins(rowPinsInOrder),
+      _colPins(colPinsInOrder),
+      _facets{Facet3{0, 0}, Facet3{5, 5}, Facet3{5, 0}, Facet3{5, 5}} {}
 
 pair<uint8_t, array<pair<uint8_t, bool>, 8>> Dcmatrix::getLine(uint8_t lineNum) {
   array<pair<uint8_t, bool>, 8> cols = {make_pair(0, false), make_pair(0, false), make_pair(0, false),
